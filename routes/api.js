82b1fdd9ae5f1c3d9120d2778a14a6d28224bd87
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2022-01-12 22:50:10 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -297,6 +297,30 @@ router.get('/emoji', async(req, res, next) => {
   })
 })
 
+router.get('/tiktokdl', async(req, res, next) => {
+  const url = req.query.url
+  const apikey = req.query.apikey;
+        if (apikey === undefined) return res.status(404).send({
+            status: 404,
+            message: `Input Parameter apikey`
+        });
+        let limit = await isLimit(apikey);
+        if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
+        const check = await cekKey(apikey);
+        if (!check) return res.status(403).send({
+          status: 403,
+          message: `apikey ${apikey} not found, please register first!`
+      });
+      limitAdd(apikey);
+  if (!url) return res.json(loghandler.invalidlink);
+  Tiktok(url)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 router.get('/otakudesu', async(req, res, next) => {
   const apikey = req.query.apikey;
         if (apikey === undefined) return res.status(404).send({
