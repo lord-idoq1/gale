a32285f1d2d563fb90695656d82dbe3bdca94ae0
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2022-04-14 23:47:51 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ const path = require('path');
 const { readFileTxt, readFileJson } = require('../lib/function');
 const { ytMp4, ytMp3, ytPlay } = require('../lib/youtube');
 const { savetik } = require('../lib/savetik');
+const { fb } = require('../lib/fbdl');
 const { cekKey, limitAdd, isLimit } = require('../database/db');
 const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
 const { cakLontong, bijak, quotes, fakta, ptl, motivasi } = require('../controllers/randomtext');
@@ -303,6 +304,30 @@ router.get('/ig/download', async(req, res, next) => {
       limitAdd(apikey);
         if(!url) return res.json(loghandler.invalidlink)
         maker.instagram(url)
+            
+ .then(data =>{ res.send(data)})
+  .catch(err=>{
+  console.log(err)
+  res.send('error')
+  })
+})
+router.get('/fb/download', async(req, res, next) => {
+        const url = req.query.url
+        const apikey = req.query.apikey;
+        if (apikey === undefined) return res.status(404).send({
+            status: 404,
+            message: `Input Parameter apikey`
+        });
+        let limit = await isLimit(apikey);
+        if (limit) return res.status(403).send({status: 403, message: 'your limit is 0, reset every morning'});
+        const check = await cekKey(apikey);
+        if (!check) return res.status(403).send({
+          status: 403,
+          message: `apikey ${apikey} not found, please register first!`
+      });
+      limitAdd(apikey);
+        if(!url) return res.json(loghandler.invalidlink)
+        fb(url)
             
  .then(data =>{ res.send(data)})
   .catch(err=>{
